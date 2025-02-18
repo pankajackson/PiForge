ab8c408b7b79a4f78a4f5de87019b15470fe57ad
Pankaj Jackson <pankajackson@live.co.uk>
2025-02-18 16:33:16 +0530

    updated post-boot script

--- post-boot.sh
+++ post-boot.sh
@@ -8,6 +8,15 @@ USER_PASS="123"
 sudo useradd -m -s /bin/bash "$USER_NAME" || echo "User $USER_NAME already exists."
 echo "$USER_NAME:$USER_PASS" | sudo chpasswd
 sudo usermod -aG sudo "$USER_NAME"
+sudo -u "$USER_NAME" xdg-user-dirs-update
+
+# Set up hostname
+sudo hostnamectl set-hostname "pi.lxa.local"
+
+# Install necessary packages
+sudo apt-get update
+sudo apt-get install -y git iwd python-pip
+sudo pip install --upgrade pip setuptools wheel pigpio
 
 # Set up keyboard layout
 sudo localectl set-keymap "us" --no-convert
